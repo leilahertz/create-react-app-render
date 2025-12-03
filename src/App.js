dd73778c241505c3b5ba9c2114ee5e925bc2266e
Leila Hertz <leilahertz@gmail.com>
2025-12-03 12:52:42 -0500

    commit for stg

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React with Leila
+            Learn React with Leila cuz now we're in staging
           </a>
         </header>
       </div>
